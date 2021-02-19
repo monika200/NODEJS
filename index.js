7156ffe22f5ecac69f967b4b721016bcc6820360
Monika Sharma <monika2004sharma@gmail.com>
2021-02-19 10:24:44 -0800

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 //app.use(bodyParser.json())
 const cors =   require("cors");
 app.use(cors({
-    origin:"http://127.0.0.1:5500"
+    origin:"https://nodejsproject.netlify.app/"
 }))
 
 const mongodb = require("mongodb");
